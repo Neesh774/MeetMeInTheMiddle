a18a44da126d5b44157b406ea23c0d7b75ee8238
Neesh774 <Kanishq0106@gmail.com>
2022-10-14 19:13:30 -0400

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import MetaTags from "../components/base/MetaTags";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <GoogleAnalytics trackPageViews />
       <MetaTags
         title="Meet Me in the Middle"
         description="Find the best meeting spots right in the middle of you and all your friends."
