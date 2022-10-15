33d49a271fbb97939d9deb905f23be7f6d21c41a
Neesh774 <Kanishq0106@gmail.com>
2022-10-15 12:25:20 -0400

    fixed ads?

--- components/base/AdBanner.tsx
+++ components/base/AdBanner.tsx
@@ -1,29 +1,11 @@
-import Script from "next/script";
-import React, { useEffect } from "react";
+import GoogleAdsense from "next-google-ads";
 
-const AdBanner = () => {
-  useEffect(() => {
-    try {
-      // @ts-ignore
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-
+export default function AdBanner() {
   return (
-    <>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-format="fluid"
-        data-ad-layout-key="-fb+5w+4e-db+86"
-        data-ad-client="ca-pub-1978358606488844"
-        data-ad-slot="1537257693"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-    </>
+    <GoogleAdsense
+      client="ca-pub-1978358606488844"
+      slot="1537257693"
+      responsive="true"
+    />
   );
-};
-
-export default AdBanner;
+}
